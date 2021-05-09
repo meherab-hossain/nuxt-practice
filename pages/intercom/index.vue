e16bc46dfa278a0fda323ab5067924d8e321080d
Md. Meherab Hossain <38518474+meherab-hossain@users.noreply.github.com>
2021-05-09 14:52:02 +0600

    portfolio base structure added

---
+++ pages/intercom/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-green-200 h-screen flex flex-col items-end">
+    <div class="bg-indigo-200 flex-1 mt-16 mr-4 m-">message</div>
+    <div class="bg-blue-600 rounded-full p-4 content-center m-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/portfolio/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bg-gray-500">
+    <div class="">
+      <div class="flex justify-between shadow-md  p-4  bg-gray-500">
+        <div class="flex justify-evenly">
+          <ul>
+            <li class="inline-flex text-white"><a>Home</a></li>
+            <li class="inline-flex menu-bar">
+              <a href="#projects">Project</a>
+            </li>
+            <li class="inline-flex menu-bar"><a>About Me</a></li>
+            <li class="inline-flex menu-bar"><a>Contact</a></li>
+            <li class="inline-flex menu-bar"><a>Resume</a></li>
+          </ul>
+        </div>
+        <div class="flex justify-evenly">
+          <div class="menu-bar">Linkdin</div>
+          <div class="menu-bar">github</div>
+          <div class="menu-bar mr-4">facebook</div>
+        </div>
+      </div>
+      <div class="flex mt-16 ml-4">
+        <div class="">
+          I am a self-motivated learner love to dive into different areas of my
+          interest and do experiment with it
+        </div>
+        <img
+          src="~/assets/meherab.png"
+          class="border object-fill h-64 w-64 rounded-full border-gray-100 shadow-sm"
+          alt="stew"
+        />
+      </div>
+      <section id="projects">
+        <div class="h-60">projects</div>
+      </section>
+      <section id="all-projects">
+        <div class="h-60"> View All Projects</div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
